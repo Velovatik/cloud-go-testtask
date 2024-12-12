9016be9fc2b42678bdf07a5f6c72c608bd58e73f
Kirill Velovatiy <kirillvelovatiy@velovatik-MacBook-Air.local>
2024-12-12 14:06:58 +0300

    refactor playlist_usecase

--- internal/usecase/playlist_usecase.go
+++ internal/usecase/playlist_usecase.go
@@ -396,3 +396,50 @@ func (uc *PlaylistUseCase) playCurrentSong() {
 		}
 	}
 }
+
+func (uc *PlaylistUseCase) AddSong(title, artist string, duration time.Duration) error {
+	const op = "usecase.SongUseCase.AddSong"
+	operationLogger := uc.logger.With(slog.String("op", op))
+
+	operationLogger.Debug("Adding new song",
+		slog.String("title", title),
+		slog.String("artist", artist),
+		slog.Duration("duration", duration),
+	)
+
+	uc.mu.Lock()
+	defer uc.mu.Unlock()
+
+	song := &entity.Song{
+		Title:    title,
+		Artist:   artist,
+		Duration: duration,
+	}
+
+	if err := uc.rdbmsRepo.AddSong(song); err != nil {
+		operationLogger.Error("Failed to add song to DB",
+			slog.String("title", title),
+			slog.String("artist", artist),
+			slog.Duration("duration", duration),
+			slog.String("error", err.Error()),
+		)
+		return fmt.Errorf("%w: %v", ErrAddSongToDB, err)
+	}
+
+	if err := uc.cacheRepo.AddSong(song); err != nil {
+		operationLogger.Error("Failed to add song to Cache",
+			slog.String("title", title),
+			slog.String("artist", artist),
+			slog.Duration("duration", duration),
+			slog.String("error", err.Error()),
+		)
+		return fmt.Errorf("%w: %v", ErrAddSongToCache, err)
+	}
+
+	operationLogger.Debug("Song added successfully",
+		slog.String("title", title),
+		slog.String("artist", artist),
+	)
+
+	return nil
+}
